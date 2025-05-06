4428703da3b53dfe5c8c2dc598a7afaad6cb4f11
jeffreyluu <jluu2003@cs.washington.edu>
2025-05-05 20:56:11 -0700

    Fixing bug test

--- src/__tests__/Chatbot.test.js
+++ src/__tests__/Chatbot.test.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import Chatbot from '../Components/Chatbot.jsx';
 
+beforeAll(() => {
+    // Jest doesn't implement scrollIntoView in JSDOM by default
+    window.HTMLElement.prototype.scrollIntoView = jest.fn();
+  });
+
 test('Chatbot renders without crashing', () => {
   render(<Chatbot />);
 });
